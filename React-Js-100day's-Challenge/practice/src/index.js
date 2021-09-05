5e91916cb7978ec6eaeb4d3ff11d6bfe633152c2
Subham <subhampandeywbjee12@gmail.com>
2021-09-06 01:02:01 +0530

    Day-1

--- React-Js-100day's-Challenge/practice/src/index.js
+++ React-Js-100day's-Challenge/practice/src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import { ReactDOM } from "react-dom";
 
-ReactDOM.render( < h1 > Hello World!! < /h1>, document.getElementById("root"))
+//Type-1
+//ReactDOM.render( < h1 > Hello World!! < /h1>, document.getElementById("root"))
+
+//Type-2
+//ReactDom.render(React.createElement("h1",null,"Hello world !!"),document.getElementById("root"))
+
+//Type-3
+//var h1=document.createElement("h1");
+//h1.innerHTML="Hello World!!";
+//document.getElementById("root").appendChild(h1);
