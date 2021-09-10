87cf1fd670648a0695151e326acc6b728c9d8b9f
Subham <subhampandeywbjee12@gmail.com>
2021-09-10 22:23:53 +0530

    Day-3

---
+++ React-Js-100days-Challenge/real-time-greetings/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let currdate= new Date();
+currdate=currdate.getHours();
+let greetings="";
+const dynamic={ };
+if(currdate>=1 && currdate<12){
+ greetings="Guten Morgen";
+ dynamic.color='green';
+}else if(currdate>=12 && currdate<20){
+  greetings="Guten Abend";
+  dynamic.color='red';
+}
+else{
+  greetings="Guten Nacht";
+  dynamic.color='gray';
+}
+
+ReactDOM.render(
+  <h1> Hallo !! <span style={dynamic}> {greetings} </span></h1>,
+  document.getElementById("root")
+);
